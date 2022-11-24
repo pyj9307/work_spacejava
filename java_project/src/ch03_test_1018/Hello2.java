20c2d24dae55ff0bd008f89468d16ebfe925af07
pyj9307 <117807055+pyj9307@users.noreply.github.com>
2022-11-24 12:33:31 +0900

    저장2

---
+++ java_project/src/ch03_test_1018/Hello2.java
@@ -0,0 +1,12 @@
+package ch03_test_1018;
+
+public class Hello2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello 이상용 자바");
+		System.out.println("Hello 이상용 자바");
+		System.out.println("Hello 이상용 자바");
+	}
+
+}
