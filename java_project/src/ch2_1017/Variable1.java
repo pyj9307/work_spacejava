db7a9a77d2b3fb0bf364c884f8770777d9b7cbd5
pyj9307 <117807055+pyj9307@users.noreply.github.com>
2022-11-24 16:38:46 +0900

    221124 5교시 수업

--- java_project/src/ch2_1017/Variable1.java
+++ java_project/src/ch2_1017/Variable1.java
@@ -7,7 +7,7 @@ public class Variable1 {
 		int level;              //정수형 변수 level을 선언
 		level = 10;		//level 변수에 10을 대입
 		// 초기화 : 최초에 변수에 값을 할당하는 것을 말함
-		byte b1 = 12; // 변수를 선언과 동시에 값은 할당
+		byte b1 = 12; // 변수를 선언과 동시에 값을 할당
 		
 		// 기본 데이터 타입 primitive type,
 		// 기본 타입을 제외한 나머지를 창조형 타입. (주로 참조형을 많이 사용)
@@ -18,7 +18,19 @@ public class Variable1 {
 		// p1 = new Person(); 기본 생성자 호출
 		// 생성자의 역할은 값을 초기화 하는 역할
 		
-		System.out.println(level); 
+		System.out.println(level);
+		System.out.println("내용 테스트 중");
+		System.out.println("단축기 설정이 장 되었음.");
+		System.out.println("간혹 단축기 실행이 안된다면 설정에서 추가해주기.");
+		// 참고 블로그 사이트
+		// https://velog.io/@susu1991/java-%EC%8B%A4%ED%96%89-%EB%8B%A8%EC%B6%95%ED%82%A4%EA%B0%80-%EC%95%88%EB%A8%B9%ED%9E%90-%EB%95%8C
+		int 상용 = 1; // 한글 이름을 사용 안함
+		int #H = 3; // 특수 문자는 $ 또는 _만 사용 가능
+		int int; // 예약어는 사용이 안됨.
+		// int i = 10000000000;
+		//해당 데이터 타입의 메모리 크기 및 공간에 대해서 암기등 이렇게 할 필요 없음.
+		// 컴파일러가 알아서 1차 문법 검사 해줍니다.
+		
 	}
 
 }
