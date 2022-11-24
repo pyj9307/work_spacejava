b991d74af2c4d183c971b133080a6352ce533256
pyj9307 <117807055+pyj9307@users.noreply.github.com>
2022-11-24 14:33:51 +0900

    221124 수업

--- java_project/src/ch03_test_1018/Hello2.java
+++ java_project/src/ch03_test_1018/Hello2.java
@@ -4,6 +4,13 @@ public class Hello2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		// 실행은 ctrl; + f11
+		// 주석이 보통은 ctrl + / : 단일 행 주석.
+		// 한번 더 ctrl + / : 취소가 됨
+		
+		// 여러 행 주석 : ctrl + shift
+		//
+		//
 		System.out.println("Hello 이상용 자바");
 		System.out.println("Hello 이상용 자바");
 		System.out.println("Hello 이상용 자바");
