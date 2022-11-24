ec857416e4271cb66f519d11b535ebd6142ddf51
pyj9307 <117807055+pyj9307@users.noreply.github.com>
2022-11-24 14:33:35 +0900

    221124 5교시 저장

--- java_project/src/ch2_1017/Variable1.java
+++ java_project/src/ch2_1017/Variable1.java
@@ -6,7 +6,18 @@ public class Variable1 {
 
 		int level;              //정수형 변수 level을 선언
 		level = 10;		//level 변수에 10을 대입
-		byte b1 = 12;
+		// 초기화 : 최초에 변수에 값을 할당하는 것을 말함
+		byte b1 = 12; // 변수를 선언과 동시에 값은 할당
+		
+		// 기본 데이터 타입 primitive type,
+		// 기본 타입을 제외한 나머지를 창조형 타입. (주로 참조형을 많이 사용)
+		// 참조형 타입 : 클래스 타입
+		// class Person
+		// 참조형 타입으로 선언하게 되면
+		// Person pl; 선언 만
+		// p1 = new Person(); 기본 생성자 호출
+		// 생성자의 역할은 값을 초기화 하는 역할
+		
 		System.out.println(level); 
 	}
 
