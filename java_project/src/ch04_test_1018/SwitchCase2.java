95b2f21993c834ce681cab907945a225c9cb8e30
pyj9307 <117807055+pyj9307@users.noreply.github.com>
2022-12-02 10:13:32 +0900

    221202 2교시 수업

---
+++ java_project/src/ch04_test_1018/SwitchCase2.java
@@ -0,0 +1,29 @@
+package ch04_test_1018;
+
+public class SwitchCase2 {
+	public static void main(String[] args) {
+
+		int ranking = 4;
+		String medal = "Silver";
+		char medalColor;
+		
+		switch(medal){
+		
+			case "Gold": medalColor = 'G';
+			ranking = 1;
+				break;
+				
+			case "Silver": medalColor = 'S';
+			ranking = 2;
+				break;
+			
+			case "Bronze": medalColor = 'B';
+			ranking = 3;
+				break;
+			default:
+				medalColor = 'A';
+		}
+		
+		System.out.println(ranking + "등 메달의 색깔은 " + medalColor + " 입니다.");
+	}
+}
